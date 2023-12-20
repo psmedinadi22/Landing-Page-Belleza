d58211d8075735b29524b7ef385b27f4105503a8
psmedinadi22 <psmedinadi@unal.edu.co>
2023-12-20 16:43:46 -0500

    menu con responsive design

--- styles/styles.js
+++ styles/styles.js
@@ -7,6 +7,7 @@ window.addEventListener('scroll', function() {
     const usLink = document.getElementById('usLink');
     const productLink = document.getElementById('productLink');
     const contactLink = document.getElementById('contactLink');
+    
   
     if (window.scrollY > 50) {
       navbar.classList.add('scrolled');
@@ -17,6 +18,7 @@ window.addEventListener('scroll', function() {
       productLink.style.color = '#4B4234';
       contactLink.style.color = '#4B4234';
       contactLink.style.borderColor = '#4B4234';
+      
     } else {
       navbar.classList.remove('scrolled');
       logoImg.src = 'images/logoBlanco.png'; 
@@ -27,10 +29,35 @@ window.addEventListener('scroll', function() {
       contactLink.style.color = '#ffffff';
       contactLink.style.borderColor = '#ffffff';
     }
+
+    const header = document.querySelector('.navbarpaula');
+    const body = document.body;
+    const contactButton = document.querySelector('.mobile-menu-icon');
+  
+    if (window.scrollY > 0) {
+      header.style.backgroundColor = '#fff'; /* Cambia el fondo del encabezado al hacer scroll */
+      body.style.backgroundColor = '#fff'; /* Cambia el fondo del cuerpo al hacer scroll */
+      body.style.color = '#4B4234'; /* Cambia el color del texto al hacer scroll */
+      contactButton.classList.add('scrolled'); /* Añade una clase para cambiar el color del ícono */
+    } else {
+      header.style.backgroundColor = 'transparent'; /* Fondo transparente cuando se encuentra en la parte superior */
+      body.style.backgroundColor = '#fff'; /* Fondo blanco cuando se encuentra en la parte superior */
+      body.style.color = '#4B4234'; /* Color de texto cuando se encuentra en la parte superior */
+      contactButton.classList.remove('scrolled'); /* Remueve la clase para restaurar el color del ícono */
+    }
+
   });
 
 
+  document.querySelector('.mobile-menu-icon').addEventListener('click', function() {
+    const menuItems = document.querySelector('.menupaula ul');
+    this.classList.toggle('open');
+    menuItems.classList.toggle('active');
+  });
+  ;
+  
 
+  
 
  
 
